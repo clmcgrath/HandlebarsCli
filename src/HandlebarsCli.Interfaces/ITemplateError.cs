108c9a1ceb8529f53ce82a9760da8416f180b3cc
Chris McGrath <chris.mcgrath@digitalparadox.ca>
2017-07-24 18:36:25 -0400

    clean up extra files

---
+++ src/HandlebarsCli.Interfaces/ITemplateError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DigitalParadox.HandlebarsCli.Interfaces
+{
+    public interface ITemplateError
+    {
+        string Description { get; set; }
+        string Name { get; set; }
+        Exception Exception { get; set; }
+    }
+}
